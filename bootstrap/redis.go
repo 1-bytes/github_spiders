75502f3082b93fabb9089b1f4bbaee916d88d84e
Enzo <admin@tcp.so>
2022-03-13 14:01:04 +0800

    redis 配置调整

--- bootstrap/redis.go
+++ bootstrap/redis.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"fmt"
 	"github.com/gocolly/redisstorage"
 	"github_spiders/pkg/collectors"
 	"github_spiders/pkg/config"
@@ -10,7 +11,10 @@ import (
 // SetupCollyRedis 初始化 Colly Redis.
 func SetupCollyRedis() {
 	storage := &redisstorage.Storage{
-		Address:  config.GetString("redis.github.address"),
+		Address: fmt.Sprintf("%s:%s",
+			config.GetString("redis.github.host"),
+			config.GetString("redis.github.port"),
+		),
 		Password: config.GetString("redis.github.password"),
 		DB:       config.GetInt("redis.github.db"),
 		Prefix:   config.GetString("redis.github.prefix"),
--- config/redis.go
+++ config/redis.go
@@ -7,7 +7,8 @@ import (
 func init() {
 	config.Add("redis", config.StrMap{
 		"github": map[string]interface{}{
-			"address":  config.Env("GITHUB_REDIS_ADDRESS", "127.0.0.1"),
+			"host":     config.Env("GITHUB_REDIS_HOST", "127.0.0.1"),
+			"port":     config.Env("GITHUB_REDIS_PORT", "6379"),
 			"password": config.Env("GITHUB_REDIS_PASSWORD", ""),
 			"db":       config.Env("GITHUB_REDIS_DB", 0),
 			"prefix":   config.Env("GITHUB_REDIS_PREFIX", ""),
